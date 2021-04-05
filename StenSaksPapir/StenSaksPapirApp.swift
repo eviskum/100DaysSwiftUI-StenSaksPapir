ba1a9b8a6c2db2c3a8f3ea1d5c20dc076eaaaf78
Esben Viskum <viskum@viskum.dk>
2021-04-05 17:22:02 +0200

    Initial Commit

---
+++ StenSaksPapir/StenSaksPapirApp.swift
@@ -0,0 +1,17 @@
+//
+//  StenSaksPapirApp.swift
+//  StenSaksPapir
+//
+//  Created by Esben Viskum on 05/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct StenSaksPapirApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
